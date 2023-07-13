75cc276f5acb9eea0165760080a05452bc18c53e
Manuel Figueroa <manuelfigueroa402@gmail.com>
2023-07-13 04:57:49 -0400

    add default answer to prompt

--- src/index.js
+++ src/index.js
@@ -10,44 +10,60 @@ inquirer
   .prompt({
     type: "input",
     name: "directoryName",
-    message: "Enter directory name:",
+    message: "Enter folder name:",
+    default: "MyAwesomeSketch",
   })
   .then((answer) => {
     fs.mkdir(path.join(process.cwd(), answer["directoryName"]), (err) => {
       if (err) {
         return console.error(err);
       }
-      fs.cp(
-        "./src/templates",
-        `./${answer["directoryName"]}`,
-        { recursive: true },
-        (err) => {
-          if (err) {
-            console.error(err);
-          }
+      const directoryName = `./${answer["directoryName"]}`;
+      fs.cp("./src/templates", directoryName, { recursive: true }, (err) => {
+        if (err) {
+          console.error(err);
         }
-      );
-      axios.get(repoUrl).then(async (res) => {
-        console.log(res.data.assets[1]["name"]);
-        console.log(res.data.assets[3]["name"]);
-        const p5MinDownloadLink = res.data.assets[1].browser_download_url;
-        const p5SoundMinDownloadLink = res.data.assets[3].browser_download_url;
-        await downloadP5(res.data.assets[1]["name"], p5MinDownloadLink);
-        await downloadP5(res.data.assets[3]["name"], p5SoundMinDownloadLink);
       });
-
-      console.log("Directory created successfully!");
+      axios
+        .get(repoUrl)
+        .then(async (res) => {
+          const p5MinDownloadLink = res.data.assets[1].browser_download_url;
+          const p5SoundMinDownloadLink =
+            res.data.assets[3].browser_download_url;
+          await downloadP5(
+            res.data.assets[1]["name"],
+            p5MinDownloadLink,
+            directoryName
+          );
+          await downloadP5(
+            res.data.assets[3]["name"],
+            p5SoundMinDownloadLink,
+            directoryName
+          );
+        })
+        .then(() => {
+          console.log("Directory created successfully!");
+        })
+        .catch((err) => {
+          throw new Error(err);
+        });
     });
   })
   .catch((err) => {
-    console.error(err);
+    throw new Error(err);
   });
 
-async function downloadP5(name, downloadLink) {
+async function downloadP5(name, downloadLink, directoryName) {
   const p5MinResponse = await axios.get(downloadLink, {
     responseType: "text",
   });
-  fs.writeFile(`${name}`, p5MinResponse.data, (err) => {
-    console.log(err);
-  });
+  fs.writeFile(
+    `${directoryName}/libraries/${name}`,
+    p5MinResponse.data,
+    (err) => {
+      if (err) {
+        throw new Error(err);
+      }
+    }
+  );
 }
